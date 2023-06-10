895775e8edf24a5e3108051e75b9a8848c98d37b
nmennella <mennella.nick@gmail.com>
2023-06-10 12:56:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,33 @@
 window.addEventListener('DOMContentLoaded', (event) => {
   const userForm = document.getElementById('userForm');
 
-  userForm.addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent form submission
+  if (userForm) {
+    userForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
 
-    // Get input values
-    const firstName = document.getElementById('firstName').value;
-    const lastName = document.getElementById('lastName').value;
+      // Get input values
+      const firstName = document.getElementById('firstName').value;
+      const lastName = document.getElementById('lastName').value;
 
-    // Identify call to Segment
-    analytics.identify('user123', {
-      first_name: firstName,
-      last_name: lastName
+      // Identify call to Segment
+      analytics.identify('user123', {
+        first_name: firstName,
+        last_name: lastName
+      });
+
+      // Track call to Segment
+      analytics.track('Form Submitted', {
+        user_id: 'user123'
+      });
+
+      // Reset form
+      userForm.reset();
     });
-
-    // Reset form
-    userForm.reset();
-  });
+  } else {
+    console.error('Error: userForm element not found');
+  }
 });
+
 
 
 
