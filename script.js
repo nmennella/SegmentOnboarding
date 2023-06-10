f407ea96105bec55c6a50cfbf0499cfb1638ed74
nmennella <mennella.nick@gmail.com>
2023-06-10 12:25:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // script.js
 window.addEventListener('DOMContentLoaded', (event) => {
-    const myButton = document.getElementById('myButton');
-  
-    myButton.addEventListener('click', function() {
-      // Actions to perform when the button is clicked
-      console.log('Button clicked!');
+  const myButton = document.getElementById('myButton');
+
+  myButton.addEventListener('click', function() {
+    // Actions to perform when the button is clicked
+    console.log('Button clicked!');
+
+    // Identify call to Segment
+    analytics.identify('user123', {
+      name: 'John Doe',
+      email: 'johndoe@example.com'
+      // additional user properties can be added here
     });
   });
+});
+
