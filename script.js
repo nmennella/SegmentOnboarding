04dbe2f227181a47ce37013d547882809f1d897e
nmennella <mennella.nick@gmail.com>
2023-06-10 12:18:51 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js
+window.addEventListener('DOMContentLoaded', (event) => {
+    const myButton = document.getElementById('myButton');
+  
+    myButton.addEventListener('click', function() {
+      // Actions to perform when the button is clicked
+      console.log('Button clicked!');
+    });
+  });
